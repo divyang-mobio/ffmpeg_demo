091e4de80d7210aa85c8e6adf3791f0526e6be82
divyang-mobio <107481877+divyang-mobio@users.noreply.github.com>
2022-09-19 16:50:45 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'dart:io';
+
+// import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
 
 void main() => runApp(const MyApp());
 
@@ -40,19 +44,43 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 ffmpegTest() async {
-  String audio = '/storage/emulated/0/Download/audio.mp3';
-  String video = '/storage/emulated/0/Download/video.mp4';
-  String image = '/storage/emulated/0/Download/image.jpg';
-  // String output = '/storage/emulated/0/Download/output.mp4';
+  String audio = 'assets/Bhool_Bhulaiyaa_2.mp3';
+  String image = 'assets/castle.jpg';
+  String video = 'assets/file_example.mp4';
+  String output = '/storage/emulated/0/Download/output.mp3';
 
   await Permission.storage.request();
   var status = await Permission.storage.status;
   if (status.isGranted) {
-    String command = '-i $audio -vf fps=30 $image -hide_banner';
+    edit(video);
 
-    FFmpegKit.executeAsync(command).then((session) async {
-      final returnCode = await session.getReturnCode();
-      print(returnCode);
-    });
+    //   // String command = '-i $video -vf "drawtext="fontfile=TiktokFont.ttf:text=\'Stack Overflow\':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=(w-text_w)/2:y=(h-text_h)/2"" -codec:a copy $output';
+    //
+    // // String command ="-i $video -r 1 -f image2 $output";//"-i $audio -ss 00:01:54 -to 00:02:53 -c copy $output"; // '-i $audio -vf fps=30 $image -hide_banner';
+    // String command = "\$ ffmpeg -i $video";
+    final appDirectory = await getExternalStorageDirectory();
+    var cmd = "-i $video -i $audio -c copy ${appDirectory?.path}/output.mp4";
+    //
+
+    // await fFmpeg.execute(cmd).then((rc) {
+    //   debugPrint("FFmpeg process exited with re: $rc");
+    // });
+
+    // // var data = ["-ss", "00:00:00" + ".00", "-t", "2:01:00" + ".00", "-noaccurate_seek", "-i", video, "-codec", "copy", "-avoid_negative_ts", "1", output];
+    //   FFmpegKit.execute(cmd).then((session) async {
+    //     final returnCode = await session.getReturnCode();
+    //     print(returnCode);
+    //   });
   }
+}
+
+edit(String videoPath) async {
+  FlutterFFmpeg fFmpeg = FlutterFFmpeg();
+  Directory appDocDir = await getApplicationDocumentsDirectory();
+  String outputPath = (appDocDir.absolute.path + '/o.mp4').toString();
+  String command = '-i $videoPath -vf curves=vintage -y $outputPath';
+  await fFmpeg.execute(command).then((rc) {
+    debugPrint("FFmpeg process exited with re: $rc");
+  });
+  debugPrint(outputPath);
 }
